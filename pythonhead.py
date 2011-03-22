3916a7555fcb3e0bfde35512e87c885700600ea8
James Booth <jamesbooth2000@hotmail.com>
2011-03-22 23:53:29 +0000

    Started swap

--- pythonhead.py
+++ pythonhead.py
@@ -6,15 +6,23 @@ def createBigEnoughDeck(numps, numcs):
     while len(deck) < cardsNeeded:
         deck.extend(deck)
 
+def clearScreen():
+    print "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+    print "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+    print "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+
+
+def charToBool(c):
+    return c.upper() == 'Y'
+
 suits = ["HEARTS", "SPADES", "DIAMONDS", "CLUBS"]
 ranks = ["TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE", "TEN", "JACK", "QUEEN", "KING", "ACE"]
 
 deck = [(x,y) for y in suits for x in ranks]
-
+pile = []
+burnt = []
 players = []
 
-print deck
-print
 
 print "Deck size =", len(deck)
 
@@ -29,19 +37,20 @@ print
 createBigEnoughDeck(numPlayers, numCardsEach)
 shuffle(deck)
 
-
 print deck
-
 print
 print len(deck)
 
 for i in range(numPlayers):
     playerName = raw_input("Enter name of player " + str(i+1) +  ": ")
     player = Player(playerName)
+    
+    for j in range(numCardsEach):
+        player.hand.append(deck.pop())
+        player.faceup.append(deck.pop())
+        player.facedown.append(deck.pop())
+
     players.append(player)
-
-players[0].addToHand([deck[0]])
-
 
 for player in players:
     print player.name
@@ -49,4 +58,34 @@ for player in players:
     print player.faceup
     print player.facedown
 
+print "Dealt cards"
+print "Cards remaining: ", len(deck)
+
+raw_input("Press enter")
+
+for player in players:
+    clearScreen()
+    print "HAND:"
+    print player.hand
+    print "FACEUP:"
+    print player.faceup
+    print
+
+    swap = charToBool(raw_input(player.name + " do you want to swap cards?"))
+
+    if swap:
+        clearScreen()
+        print player.name
+        print
+        print "HAND:"
+        print player.hand
+        print "FACEUP:"
+        print player.faceup
+        print
+
+        hCard = int(raw_input("Which card from your hand do you wish to swap? "))
+    
+        fCard = int(raw_input("Which card from your hand do you wish to swap? "))
+       
+    raw_input()
 
